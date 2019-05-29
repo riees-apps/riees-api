b6f80dd6513a07c860f4d875700be118db014583
Ericles Brum <35080587+Frakren@users.noreply.github.com>
2019-05-29 11:13:05 -0300

    Instituição
    Foram feitas
    Adicionar, Editar e visualizar

---
+++ api/models/Instituicao.js
@@ -0,0 +1,55 @@
+/**
+ * Instituicao.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    name: {
+      type: 'string',
+      minLength: 8,
+      maxLength: 64,
+      required: true,
+      example: 'Universidade Vila Velha'
+    },
+    mission: {
+      type: 'string',
+      minLength: 8,
+      maxLength: 128,
+      required: true,
+      example: 'Missao'
+    },
+    description: {
+      type: 'string',
+      minLength: 8,
+      maxLength: 512,
+      required: true,
+      example: 'descricao'
+    },
+    advantages: {
+      type: 'string',
+      minLength: 8,
+      maxLength: 16,
+      required: true,
+      example: 'pros'
+    }
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
